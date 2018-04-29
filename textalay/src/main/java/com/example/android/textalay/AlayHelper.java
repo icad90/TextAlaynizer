f2eae9b2b5a38f92819762f07a24abe5510b93af
Icad <muhammad.irsyad28@gmail.com>
2018-04-29 14:36:54 +0700

    test 2

---
+++ textalay/src/main/java/com/example/android/textalay/AlayHelper.java
@@ -0,0 +1,24 @@
+package com.example.android.textalay;
+
+public class AlayHelper {
+
+    public static String alaynize(String input){
+        String result = "";
+        result = result.replaceAll("A", "4");
+        result = result.replaceAll("a", "4");
+        result = result.replaceAll("e", "3");
+        result = result.replaceAll("E", "3");
+        result = result.replaceAll("I", "1");
+        result = result.replaceAll("i", "5");
+        result = result.replaceAll("S", "5");
+        result = result.replaceAll("s", "5");
+        result = result.replaceAll("G", "6");
+        result = result.replaceAll("g", "6");
+        result = result.replaceAll("J", "7");
+        result = result.replaceAll("j", "7");
+        result = result.replaceAll("B", "8");
+        result = result.replaceAll("b", "8");
+        result = result.replaceAll("nya", "x");
+        return result ;
+    }
+}
